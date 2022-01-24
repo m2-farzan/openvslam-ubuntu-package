356315cbb9cf17ad5d3aa9c9bcce5b7d2915850c
Mostafa Farzan <farzan@cakerobotics.com>
2022-01-25 01:16:04 +0330

    Install cmake discovery files in /usr/lib/cmake

--- pack.bash
+++ pack.bash
@@ -1,3 +1,4 @@
-mkdir DEBIAN/openvslam-community/opt
+mkdir -p DEBIAN/openvslam-community/opt DEBIAN/openvslam-community/usr/lib/cmake
+cp -r /opt/openvslam-community/lib/cmake/openvslam DEBIAN/openvslam-community/usr/lib/cmake/
 mv /opt/openvslam-community DEBIAN/openvslam-community/opt/
 dpkg-deb --build openvslam-community
