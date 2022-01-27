1bcc6fec24fb3b1687fdd5f03c9b11c6c1a73669
M. Mostafa Farzan <m2_farzan@yahoo.com>
2022-01-27 03:41:30 +0330

    Fix pack destination

--- pack.bash
+++ pack.bash
@@ -3,7 +3,7 @@ set -e
 mkdir -p opt usr/lib/cmake usr/bin
 cp -r /opt/openvslam-community/lib/cmake/openvslam usr/lib/cmake/
 cp -r /opt/openvslam-community opt/
-cp ws/openvslam/build/lib/*.so /opt/openvslam-community/lib/
+cp ws/openvslam/build/lib/*.so opt/openvslam-community/lib/
 pushd ws/openvslam/build
 ls run_* | xargs -I% cp % ../../../usr/bin/openvslam-%
 popd
