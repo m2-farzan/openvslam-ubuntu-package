2af4bd3e0954a49dc7cb7f9afb89ad4597a88540
Mostafa Farzan <farzan@cakerobotics.com>
2022-01-29 04:29:07 +0330

    Add 'test' job

--- test.bash
+++ test.bash
@@ -1,5 +1,14 @@
 #!/bin/bash
 set -e
-pushd ws/openvslam/build/test
-make test ARGS="-V"
-popd
+mkdir ws
+cd ws
+git clone --branch 0.2.4 https://github.com/OpenVSLAM-Community/openvslam
+cd openvslam
+git submodule update -i --recursive
+git apply ../../openvslam.patch
+git apply ../../test-build.patch
+cd test
+mkdir build
+cd build
+cmake -DCMAKE_INSTALL_PREFIX=/opt/openvslam-community -DBOW_FRAMEWORK=DBoW2 ..
+ls test_* | xargs -I% bash -c ./%
