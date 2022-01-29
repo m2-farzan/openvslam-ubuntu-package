a59937305e524d9aa9ffbc7cb65b4b8dd3c7da0a
Mostafa Farzan <farzan@cakerobotics.com>
2022-01-29 19:30:32 +0330

    Remove extra files before packaging

--- pack.bash
+++ pack.bash
@@ -6,5 +6,7 @@ cp ws/openvslam/build/lib/*.so opt/openvslam-community/lib/
 pushd ws/openvslam/build
 ls run_* | xargs -I% cp % ../../../usr/bin/openvslam-%
 popd
-rm -rf ws
+shopt -s extglob
+shopt -s dotglob
+rm -rfv ../openvslam-ubuntu-package/!(opt|usr)  # redundant relative patch is used for safety
 dpkg-deb --build $(pwd)
